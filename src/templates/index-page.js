daa02026938cd9f1698534fdcb2799cfdfc05d2b
Nick Meincken <nick.meincken@gmail.com>
2020-02-17 18:09:23 +0000

    fix

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { graphql, Link } from "gatsby";
-import { GlobalStyle } from "../shared/Global";
+import { GlobalStyle } from "../shared/global";
 import { color } from "../shared/styles";
 
 import Layout from "../components/Layout";
