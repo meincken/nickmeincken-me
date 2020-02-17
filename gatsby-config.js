6ff26e0199c4fdb159ad148c40fb49c29a27d8c0
Nick Meincken <nick.meincken@gmail.com>
2020-02-17 16:05:23 +0000

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-plugin-styled-components`,
+      resolve: "gatsby-plugin-styled-components",
       options: {
         // Add any options here
       }
