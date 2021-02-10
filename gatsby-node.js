83df113b927744e8251d839cc5ce5ef91c0f8dfd
meincken <nick@meincken.com>
2021-02-10 21:20:18 +0000

    fiix lodash

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-const _ = require("lodash");
+import { kebabCase } from "lodash";
 const path = require("path");
 const { createFilePath } = require("gatsby-source-filesystem");
 const { fmImagesToRelative } = require("gatsby-remark-relative-images");
